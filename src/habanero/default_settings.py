b3fb7a984e1484e9e04440f33a5ad0fff5f514d9
Pablo SEMINARIO <pablo@seminar.io>
2016-02-28 01:51:15 +0100

    Added `auth` view.

--- src/habanero/default_settings.py
+++ src/habanero/default_settings.py
@@ -1,1 +1,26 @@
 DEBUG = False
+
+# You must define API_KEYS in your local settings
+# Using the following format:
+#
+# API_KEYS = {
+#     'api-key-1': ('username-1', {
+#         'api-secret-1': 'app-name-1',
+#         'api-secret-2': 'app-name-2',
+#     }),
+#     'api-key-2': ('username-2', {
+#         'api-secret-1': 'app-name-1',
+#         'api-secret-2': 'app-name-2',
+#     }),
+# }
+#
+# Here's an example:
+#
+# API_KEYS = {
+#     'DqXH3pg2dJfBma': ('pabluk', {
+#         'd73c60243179107aecfbcb6204066a5af79f042e': 'Tamales',
+#         'aaa7bf68cb722c1680165f991db3f0371de438f6': 'Guacamole',
+#     }),
+# }
+#
+API_KEYS = {}
--- src/habanero/views.py
+++ src/habanero/views.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from habanero import app, __version__
 
@@ -11,3 +11,20 @@ def index():
         'url': 'http://habanero.antojitos.io/',
     }
     return jsonify(**data)
+
+
+@app.route("/api/v1/auth", methods=['GET'])
+def auth():
+    api_keys = app.config['API_KEYS']
+    key = request.headers.get('x-api-key')
+    secret = request.headers.get('x-api-secret')
+
+    if key in api_keys:
+        username, secrets = api_keys[key]
+        if secret in secrets:
+            app_name = secrets[secret]
+            message = 'User %s authenticated for %s.' % (username, app_name)
+            return jsonify({'message': message}), 200
+
+    message = 'Authentication credentials were missing or incorrect.'
+    return jsonify({'message': message}), 401
